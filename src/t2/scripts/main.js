8b34bc0f2863661a19fcf6edadee798410db4fab
CristianoAndrade <cristianoandrade89@hotmail.com>
2019-05-26 18:52:38 -0300

    Layout T2 finalizado

---
+++ src/t2/scripts/main.js
@@ -0,0 +1,1 @@
+  console.log('Implemente o layout que se encontra na pasta design de forma responsiva, utilizando html5 e as boas práticas do front end. O site será testado em todos os navegadores e também em dispositivos móveis');
