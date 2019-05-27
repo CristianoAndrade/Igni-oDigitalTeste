704aa66851055c91079f3a5bceda0b040c3ed11a
CristianoAndrade <cristianoandrade89@hotmail.com>
2019-05-27 01:14:01 -0300

    Adição do arquivo Json e alteração do index exercicio t2

--- src/t2/scripts/main.js
+++ src/t2/scripts/main.js
@@ -1,1 +1,13 @@
   console.log('Implemente o layout que se encontra na pasta design de forma responsiva, utilizando html5 e as boas práticas do front end. O site será testado em todos os navegadores e também em dispositivos móveis');
+
+$.get("http://localhost/server",selecionaImagem);
+
+function selecionaImagem(imagens) {
+  $('.learn').append(`<img src="${imagens.imagens[4].imagem}" alt="">`);
+
+  $(".banner").append(`<img src="${imagens.imagens[0].imagem}" alt="Banner">`);
+
+  $(".imagens").append(`<img src="${imagens.imagem[1].imagem}" alt="Imagem 1" class="col">
+    <img src="${imagens.imagem[2].imagem}" alt="Imagem 2" class="col">
+    <img src="${imagens.imagem[3].imagem}" alt="Imagem 2" class="col">`);
+}
