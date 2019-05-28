db6b73373da1ce318a479b950478ec70e5ea6bc5
CristianoAndrade <cristianoandrade89@hotmail.com>
2019-05-28 01:44:52 -0300

    finalização T3

---
+++ src/t3/scripts/index.js
@@ -0,0 +1,8 @@
+  console.log('Faça com que o box-move passe pelas 4 arestas das bordas vermelhas \n com as seguintes regras:');
+
+  console.log('Quando o box-move estiver na aresta B ficará da cor #0F0, quando estiver na aresta C ficará da cor #00F \n no ponto D o elemento se tornará um quadrado e ficará da cor #FFF; ');
+
+  console.log('Após isso o box-move deve voltar ao seu ponto de origem seguindo as mesmas regras ao contrário até se tornar um círculo. ');
+  console.log('A animação deve ser infinita \n');
+  console.log('Deve ser utilizado apenas css para fazer estar animação.\n\n');
+  console.log('Dica: keyframes');
